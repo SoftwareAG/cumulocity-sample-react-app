021bfe43d31e5090e0ee45eea00f5e4717a5077f
Shilpy Khanna (y509125) <Shilpy.Khanna@softwareag.com>
2021-11-15 20:38:08 +0530

    removed some unwanted code

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,19 @@ import { BasicAuth, Client } from '@c8y/client';
 import { FetchDeviceDetails } from 'sample-react-library1/lib';
 
 function App() {
-  const auth = new BasicAuth({
-    user: 'shilpy.khanna@softwareag.com',
-    password: 'Cumulocity@123#',
-    tenant: 'env668860'
+  const  auth = new  BasicAuth({
+    user:  'username',
+    password:  'password',
+    tenant:  'tenantId'
     });
-     
-    const client = new Client(auth, 'https://env668860.us.cumulocity.com');
+    
+    const  client = new  Client(auth, 'tenantUrl');
     client.setAuth(auth);
-    const fetchClient = client.core;
+    const  fetchClient = client.core;
 
   return (
     <div>
-    <div>This is a react app. Below is the react library widget</div>
-    <div>..................................................................................</div>
     <FetchDeviceDetails fetchClient = {fetchClient} id={'2170'}></FetchDeviceDetails>
-    <div>.....................................................................................</div>
     </div>
   );
 }
