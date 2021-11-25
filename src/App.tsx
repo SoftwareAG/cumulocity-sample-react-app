cd75c904ca9a2662478c8e1564192867257b0577
Shilpy Khanna (y509125) <Shilpy.Khanna@softwareag.com>
2021-11-25 11:19:25 +0530

    Removed user credentials

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
 import './App.css';
 import { BasicAuth, Client } from '@c8y/client';
-import { FetchDeviceDetails } from 'sample-react-library-widget/lib';
+import { FetchDeviceDetails } from 'sample-react-library-widget';
 
 function App() {
-  const  auth = new  BasicAuth({
-    user:  'username',
-    password:  'password',
-    tenant:  'tenantId'
-    });
-    
-    const  client = new  Client(auth, 'tenantUrl');
-    client.setAuth(auth);
-    const  fetchClient = client.core;
+  const auth = new BasicAuth({
+    user: 'username',
+    password: 'password',
+    tenant: 'tenantId'
+  });
+  const client = new Client(auth, 'tenantUrl');
+  client.setAuth(auth);
+  const fetchClient = client.core;
 
   return (
     <div>
-    <FetchDeviceDetails fetchClient = {fetchClient} id={'2170'}></FetchDeviceDetails>
+    <FetchDeviceDetails fetchClient = {fetchClient} id={'deviceId'}></FetchDeviceDetails>
     </div>
   );
 }
